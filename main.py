f61c7d24b59d66b42a3c94a6ce9e9883eb285dd0
sovmulax <soumailaevane@gmail.com>
2022-10-19 10:34:13 +0000

    commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import time
+
+def tri_selection(tab):
+    #début timer
+    start = time.time_ns()
+
+    for i in range(len(tab)):
+
+       # Trouver le min
+        min = i
+
+        for j in range(i+1, len(tab)):
+            if tab[min] > tab[j]:
+                min = j
+
+        tmp = tab[i]
+        tab[i] = tab[min]
+        tab[min] = tmp
+
+    #fin timer
+    end = time.time_ns()
+
+    nano = format(end-start)
+    nano_float = float(nano)
+    print(" temps d'exécution : " + str(nano_float*0.000000001) + " s")
+
+    return tab
+
+tab = [20, 30, 15, 18, 7, 19, 21, 31, 81, 90]
+print("Le tableau de base est: ")
+print(tab)
+
+tri_selection(tab)
+
+print("Le tableau trié est: ")
+print(tab)
